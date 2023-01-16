a53cd8ae14323abbcd991d7093698df41be1416e
Uwumuhire Rene <reneuwumuhire@gmail.com>
2023-01-16 20:53:18 +0200

    chore: initial settup

--- src/scenes/global/TopBar.jsx
+++ src/scenes/global/TopBar.jsx
@@ -15,7 +15,35 @@ const TopBar = () => {
 	const colorMode = useContext(ColorModeContext);
 	return (
 		<Box display="flex" justifyContent="space-between" p={2}>
-			Topbar
+			{/* search bar */}
+			<Box
+				display="flex"
+				backgroundColor={colors.primary[400]}
+				borderRadius="3px">
+				<InputBase sx={{ ml: 2, flex: 1 }} placeholder="search" />
+				<IconButton type="button" sx={{ p: 1 }}>
+					<SearchIcon />
+				</IconButton>
+			</Box>
+			{/* icons */}
+			<Box display="flex">
+				<IconButton onClick={colorMode.toggleColorMode}>
+					{theme.palette.mode === "dark" ? (
+						<DarkModeOutlinedIcon />
+					) : (
+						<LightModeOutlinedIcon />
+					)}
+				</IconButton>
+				<IconButton>
+					<NotificationsOutlinedIcon />
+				</IconButton>
+				<IconButton>
+					<SettingsOutlinedIcon />
+				</IconButton>
+				<IconButton>
+					<PersonOutlineOutlinedIcon />
+				</IconButton>
+			</Box>
 		</Box>
 	);
 };
